bcdb420094920cf3d6c1a5a8a413d25df169a7cd
Finley Lau <finleylau98@gmail.com>
2018-07-11 09:57:55 -0500

    finished

--- assignment3.py
+++ assignment3.py
@@ -1,17 +1,11 @@
 def addTwo(n):
   # Adds 2 to a number
-  # TODO
-
-  return n
+  return n + 2
 
 def addOne(n):
   # Adds 1 to a number
-  # TODO
-
-  return n
+  return n + 1
 
 def divide(x, y):
   # Divides x/y
-  # TODO
-
-  return 0
+  return x/y
